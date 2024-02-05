543e19e56c81155352c2f22f62a5686667ec5acf
Jan Liska <jan.liska@autodesk.com>
2024-02-05 12:16:23 +0100

    added example to list views

---
+++ views/list-views.py
@@ -0,0 +1,24 @@
+"""
+This example demonstrates how to list saved views of the facility.
+
+It uses 2-legged authentication - this requires that application is added to facility as service.
+"""
+
+from common.auth import create_token
+from common.tandemClient import TandemClient
+
+# update values below according to your environment
+APS_CLIENT_ID = 'YOUR_CLIENT_ID'
+APS_CLIENT_SECRET = 'YOUR_CLIENT_SECRET'
+FACILITY_URN = 'YOUR_FACILITY_URN'
+
+# Start
+# STEP 1 - obtain token. The sample uses 2-legged token but it would also work
+# with 3-legged token assuming that user has access to the facility
+token = create_token(APS_CLIENT_ID, APS_CLIENT_SECRET, ['data:read'])
+with TandemClient(lambda: token) as client:
+    # STEP 2 - get facility views
+    views = client.get_views(FACILITY_URN)
+    # STEP 3 - iterate through views and print their name and id
+    for view in views:
+        print(f'{view.get('viewName')}:{view.get('id')}')
