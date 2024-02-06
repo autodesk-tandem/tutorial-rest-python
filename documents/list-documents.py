ff6ce0ce76c53a74c616f5533cf5e3c31a37dbc4
Jan Liska <jan.liska@autodesk.com>
2024-02-06 10:30:59 +0100

    added example to list documents

---
+++ documents/list-documents.py
@@ -0,0 +1,27 @@
+"""
+This example demonstrates how to list linked documents from given facility.
+
+It uses 2-legged authentication - this requires that application is added to facility as service.
+"""
+
+from common.auth import create_token
+from common.tandemClient import TandemClient
+
+# update values below according to your environment
+APS_CLIENT_ID = 'YOUR_CLIENT_ID'
+APS_CLIENT_SECRET = 'YOUR_CLIENT_SECRET'
+FACILITY_URN = 'YOUR_FACILITY_URN'
+
+# Start
+# STEP 1 - obtain token. The sample uses 2-legged token but it would also work
+# with 3-legged token assuming that user has access to the facility
+token = create_token(APS_CLIENT_ID, APS_CLIENT_SECRET, ['data:read'])
+with TandemClient(lambda: token) as client:
+    # STEP 2 - get facility
+    facility = client.get_facility(FACILITY_URN)
+    docs = facility.get('docs')
+    if docs is None:
+        exit()
+    # STEP 3 - iterate through documents and print their name and id
+    for doc in docs:
+        print(f'{doc.get('name')}:{doc.get('id')}')
