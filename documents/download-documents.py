c1c7b737f083b62c72cb44af1249f50f9bd89c5c
Jan Liska <jan.liska@autodesk.com>
2024-02-06 10:50:38 +0100

    added example to download documents

---
+++ documents/download-documents.py
@@ -0,0 +1,31 @@
+"""
+This example demonstrates how to download linked documents from given facility.
+
+It uses 2-legged authentication - this requires that application is added to facility as service.
+"""
+import os
+
+from common.auth import create_token
+from common.tandemClient import TandemClient
+
+# update values below according to your environment
+APS_CLIENT_ID = 'YOUR_CLIENT_ID'
+APS_CLIENT_SECRET = 'YOUR_CLIENT_SECRET'
+FACILITY_URN = 'YOUR_FACILITY_URN'
+DOWNLOAD_PATH = os.environ.get('TEMP')
+
+# Start
+# STEP 1 - obtain token. The sample uses 2-legged token but it would also work
+# with 3-legged token assuming that user has access to the facility
+token = create_token(APS_CLIENT_ID, APS_CLIENT_SECRET, ['data:read'])
+with TandemClient(lambda: token) as client:
+    # STEP 2 - get facility
+    facility = client.get_facility(FACILITY_URN)
+    docs = facility.get('docs')
+    if docs is None:
+        exit()
+    # STEP 3 - iterate through documents
+    for doc in docs:
+        print(f'{doc.get('name')}:{doc.get('id')}')
+        # STEP 4 - download document to local file
+        client.save_document_content(doc.get('signedLink'), os.path.join(DOWNLOAD_PATH, doc.get('name')))
