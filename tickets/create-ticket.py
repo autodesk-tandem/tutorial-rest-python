77a8b04dec20ae66765587e538cc2c6d41fc3fe2
Jan Liska <jan.liska@autodesk.com>
2025-11-19 13:58:36 +0100

    updated logic to add rooms and level from parent

--- tickets/create-ticket.py
+++ tickets/create-ticket.py
@@ -10,22 +10,28 @@ from datetime import datetime, timezone
 from common.auth import create_token
 from common.tandemClient import TandemClient
 from common.constants import (
+    COLUMN_FAMILIES_REFS,
     COLUMN_FAMILIES_STANDARD,
     COLUMN_FAMILIES_XREFS,
     COLUMN_NAMES_ELEMENT_FLAGS,
+    COLUMN_NAMES_LEVEL,
     COLUMN_NAMES_NAME,
     COLUMN_NAMES_OPEN_DATE,
     COLUMN_NAMES_PARENT,
     COLUMN_NAMES_PRIORITY,
-    ELEMENT_FLAGS_ALL_LOGICAL_MASK,
+    COLUMN_NAMES_ROOMS,
     ELEMENT_FLAGS_TICKET,
     MUTATE_ACTIONS_INSERT,
     QC_ELEMENT_FLAGS,
     QC_KEY,
+    QC_LEVEL,
+    QC_OLEVEL,
     QC_NAME,
-    QC_ONAME
+    QC_ONAME,
+    QC_XROOMS,
+    QC_OXROOMS
 )
-from common.encoding import new_element_key, to_full_key, to_xref_key
+from common.encoding import decode_xref_key, to_full_key, to_xref_key
 from common.utils import get_default_model, is_logical_element
 
 # update values below according to your environment
@@ -48,8 +54,7 @@ def main():
         facility = client.get_facility(FACILITY_URN)
         default_model = get_default_model(FACILITY_URN, facility)
         if default_model is None:
-            print('Unable to find default model')
-            return
+            raise Exception('Unable to find default model')
         # STEP 3 - find asset by name. In case when there is multiple assets with same name the first one is selected.
         xref = None
 
@@ -71,9 +76,33 @@ def main():
                 break
         if xref is None:
             raise Exception(f'Unable to find asset with name: {ASSET_NAME}')
-        # STEP 4 - create ticket connected to asset
+        # STEP 4 - get rooms and levels from parent asset
+        model_id, element_key = decode_xref_key(xref)
+        rooms_ids = None
+        level_id = None
+
+        if (model_id is not None) and (element_key is not None):
+            parent_element = client.get_element(model_id, element_key,
+                                                column_families=[COLUMN_FAMILIES_STANDARD, COLUMN_FAMILIES_REFS, COLUMN_FAMILIES_XREFS])
+            rooms_ids = parent_element.get(QC_OXROOMS, None)
+            if rooms_ids is None:
+                rooms_ids =  parent_element.get(QC_XROOMS, None)
+            # STEP 5 - find level by name in default model
+            parent_level_id = parent_element.get(QC_OLEVEL, None)
+            if parent_level_id is None:
+                parent_level_id = parent_element.get(QC_LEVEL, None)
+
+            if parent_level_id is not None:
+                parent_level = client.get_element(model_id, parent_level_id, column_families=[COLUMN_FAMILIES_STANDARD])
+                level_name = parent_level.get(QC_ONAME, None)
+                if level_name is None:
+                    level_name = parent_level.get(QC_NAME, None)
+                levels = client.get_levels(default_model.get('modelId', None))
+                level = next((l for l in levels if l.get(QC_ONAME) == level_name or l.get(QC_NAME) == level_name), None)
+                if level is not None:
+                    level_id = level.get(QC_KEY, None)
+        # STEP 6 - payload for ticket connected to asset
         flags = ELEMENT_FLAGS_TICKET
-        new_key = new_element_key(flags & ELEMENT_FLAGS_ALL_LOGICAL_MASK)
         mutations = [
             [
                 MUTATE_ACTIONS_INSERT,
@@ -91,7 +120,7 @@ def main():
                 MUTATE_ACTIONS_INSERT,
                 COLUMN_FAMILIES_XREFS,
                 COLUMN_NAMES_PARENT,
-               xref
+                xref
             ],
             [
                 MUTATE_ACTIONS_INSERT,
@@ -106,13 +135,26 @@ def main():
                 datetime.now(timezone.utc).date().isoformat()
             ]
         ]
-        keys = [new_key] * len(mutations)
+        if rooms_ids is not None:
+            mutations.append([
+                MUTATE_ACTIONS_INSERT,
+                COLUMN_FAMILIES_XREFS,
+                COLUMN_NAMES_ROOMS,
+                rooms_ids
+            ])
+        if level_id is not None:
+            mutations.append([
+                MUTATE_ACTIONS_INSERT,
+                COLUMN_FAMILIES_REFS,
+                COLUMN_NAMES_LEVEL,
+                level_id
+            ])
         inputs = {
-            'keys': keys,
             'muts': mutations,
+            'desc': 'Create ticket'
         }
-        client.create_element(default_model.get('modelId'), inputs)
-        print(f'Ticket {TICKET_NAME} created and assigned to asset {ASSET_NAME}')
+        ticket_result = client.create_element(default_model.get('modelId'), inputs)
+        print(f'Created new ticket: {TICKET_NAME} ({ticket_result.get('key', None)})')
 
 if __name__ == '__main__':
     main()
